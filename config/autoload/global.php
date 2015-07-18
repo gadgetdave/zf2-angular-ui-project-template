3b83f37f173000d7f570ace68e20dfc725546c3e
Dave King <daveking.work@gmail.com>
2015-07-18 11:42:54 +0100

    create SpaceName.js and moved generic mothods into there

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -66,6 +66,7 @@ return array(
                     
                     'base_js' => array(
                         'assets' => array(
+                            'js/SpaceName.js',
                             'js/MyApp.js',
                             'js/html5shiv.min.js',
                         )
